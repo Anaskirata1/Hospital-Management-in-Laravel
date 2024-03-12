283ab8625ded1da72519c6fc0821a6e98a29dc41
Anas.K <anaskirata2@gmail.com>
2024-03-12 02:27:04 -0700

    start project and install dashboard

---
+++ routes/Backend.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\DashboardController ;
+
+/*
+|--------------------------------------------------------------------------
+| Backend Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/Dashboard_Admin', [DashboardController::class, 'index']);
