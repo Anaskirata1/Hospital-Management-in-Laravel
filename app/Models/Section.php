87057e151105d35ecd61703f53a9af229d0b240c
Anas.K <anaskirata2@gmail.com>
2024-03-14 04:04:02 -0700

    Create Table Sections & Translations

---
+++ app/Models/Section.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Translatable;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+
+
+
+class Section extends Model
+{
+    use Translatable; // 2. To add translation methods
+
+    // 3. To define which attributes needs to be translated
+    public $translatedAttributes = ['name'];
+
+    use HasFactory;
+}
